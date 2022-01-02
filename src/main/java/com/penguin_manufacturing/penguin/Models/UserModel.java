50eba6ef1ac5d615e13195368b1afc3ca39401c8
Siew Joe Kane <joekanesiew@outlook.com>
2022-01-02 12:12:16 +0800

    added lombok

--- src/main/java/com/penguin_manufacturing/penguin/Models/UserModel.java
+++ src/main/java/com/penguin_manufacturing/penguin/Models/UserModel.java
@@ -1,52 +1,30 @@
 package com.penguin_manufacturing.penguin.Models;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name = "users")
 public class UserModel {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "user_id")
-  private Number id;
-
-  @Column(name = "user_name")
+  private Number user_id;
   private String user_name;
-
-  @Column(name = "user_password")
   private String user_password;
-
-  @Column(name = "role")
-  private Number user_role;
-
-  @Column(name = "first_name")
+  private Number role;
   private String first_name;
-
-  @Column(name = "last_name")
   private String last_name;
-
-  @Column(name = "image_url")
   private String image_url;
-
-  public void setuser_name(String user_name) {
-    this.user_name = user_name;
-  }
-
-  public String getuser_name() {
-    return this.user_name;
-  }
-
-  public void setimage_url(String image_url) {
-    this.image_url = image_url;
-  }
-
-  public String getimage_url() {
-    return this.image_url;
-  }
 }
